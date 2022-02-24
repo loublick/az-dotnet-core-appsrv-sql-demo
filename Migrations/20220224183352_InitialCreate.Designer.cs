60e0be03397a2a6190d773a089d165ac9eae9023
loublick <loublick@microsoft.com>
2022-02-24 13:44:10 -0500

    Added Azure SQL migration code.

--- Migrations/20220224183352_InitialCreate.Designer.cs
+++ Migrations/20220224183352_InitialCreate.Designer.cs
@@ -10,7 +10,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace DotNetCoreSqlDb.Migrations
 {
     [DbContext(typeof(MyDatabaseContext))]
-    [Migration("20220222193918_InitialCreate")]
+    [Migration("20220224183352_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
